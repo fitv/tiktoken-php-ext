d00cd461393eb51304fecd23c97a4747ed54990a
fitv <50615483+fitv@users.noreply.github.com>
2023-08-11 12:47:35 +0800

    add encode and decode

--- src/entity.rs
+++ src/entity.rs
@@ -2,9 +2,30 @@ use ext_php_rs::convert::FromZval;
 use ext_php_rs::flags::DataType;
 use ext_php_rs::types::Zval;
 use std::collections::HashMap;
+use tiktoken_rs::tokenizer::Tokenizer;
 use tiktoken_rs::FunctionCall;
 
 pub type Message = HashMap<String, MessageValue>;
+
+pub trait MessageAccessor {
+    fn string(&self, key: &str) -> Option<String>;
+    fn function_call(&self) -> Option<FunctionCall>;
+}
+
+impl MessageAccessor for Message {
+    fn string(&self, key: &str) -> Option<String> {
+        Some(self.get(key)?.string()?)
+    }
+
+    fn function_call(&self) -> Option<FunctionCall> {
+        let function_call = self.get("function_call")?.hashmap()?;
+
+        Some(FunctionCall {
+            name: function_call.get("name")?.to_string(),
+            arguments: function_call.get("arguments")?.to_string(),
+        })
+    }
+}
 
 pub enum MessageValue {
     String(String),
@@ -34,29 +55,29 @@ impl<'a> FromZval<'a> for MessageValue {
 
 impl MessageValue {
     pub fn string(&self) -> Option<String> {
-        match self {
-            MessageValue::String(string) => Some(string.to_string()),
-            _ => None,
+        if let MessageValue::String(string) = self {
+            Some(string.to_string())
+        } else {
+            None
         }
     }
 
     pub fn hashmap(&self) -> Option<&HashMap<String, String>> {
-        match self {
-            MessageValue::HashMap(hashmap) => Some(hashmap),
-            _ => None,
+        if let MessageValue::HashMap(hashmap) = self {
+            Some(hashmap)
+        } else {
+            None
         }
     }
 }
 
-pub fn get_message_string(message: &Message, key: &str) -> Option<String> {
-    Some(message.get(key)?.string()?)
-}
-
-pub fn get_message_function_call(message: &Message) -> Option<FunctionCall> {
-    let function_call = message.get("function_call")?.hashmap()?;
-
-    Some(FunctionCall {
-        name: function_call.get("name")?.to_string(),
-        arguments: function_call.get("arguments")?.to_string(),
-    })
+pub fn get_tokenizer(tokenizer: &str) -> Option<Tokenizer> {
+    match tokenizer {
+        "gpt2" => Some(Tokenizer::Gpt2),
+        "r50k_base" => Some(Tokenizer::R50kBase),
+        "p50k_base" => Some(Tokenizer::P50kBase),
+        "p50k_edit" => Some(Tokenizer::P50kEdit),
+        "cl100k_base" => Some(Tokenizer::Cl100kBase),
+        _ => None,
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,38 @@
 
 mod entity;
 
-use entity::{get_message_function_call, get_message_string, Message};
-use ext_php_rs::prelude::{php_function, php_module, ModuleBuilder};
+use entity::{get_tokenizer, Message, MessageAccessor};
+use ext_php_rs::prelude::{php_function, php_module, ModuleBuilder, PhpResult};
 use tiktoken_rs::model::get_context_size;
-use tiktoken_rs::{get_bpe_from_model, ChatCompletionRequestMessage};
+use tiktoken_rs::{get_bpe_from_model, get_bpe_from_tokenizer, ChatCompletionRequestMessage};
+
+/// Encode the text into a list of tokens.
+#[php_function(name = "TikToken\\encode")]
+pub fn encode(tokenizer: String, text: String) -> PhpResult<Vec<usize>> {
+    let tokenizer =
+        get_tokenizer(&tokenizer).ok_or(format!("Tokenizer {} not found", tokenizer))?;
+    Ok(get_bpe_from_tokenizer(tokenizer)?.encode_with_special_tokens(text.as_str()))
+}
+
+/// Decode the tokens into a text.
+#[php_function(name = "TikToken\\decode")]
+pub fn decode(tokenizer: String, tokens: Vec<usize>) -> PhpResult<String> {
+    let tokenizer =
+        get_tokenizer(&tokenizer).ok_or(format!("Tokenizer {} not found", tokenizer))?;
+    Ok(get_bpe_from_tokenizer(tokenizer)?.decode(tokens)?)
+}
+
+/// Encode the text into a list of tokens for a specified model.
+#[php_function(name = "TikToken\\encode_for_model")]
+pub fn encode_for_model(model: String, text: String) -> PhpResult<Vec<usize>> {
+    Ok(get_bpe_from_model(model.as_str())?.encode_with_special_tokens(text.as_str()))
+}
+
+// Decode the tokens into a text for a specified model.
+#[php_function(name = "TikToken\\decode_for_model")]
+pub fn decode_for_model(model: String, tokens: Vec<usize>) -> PhpResult<String> {
+    Ok(get_bpe_from_model(model.as_str())?.decode(tokens)?)
+}
 
 /// Get the maximum token number of a specified model.
 #[php_function(name = "TikToken\\model_max_tokens")]
@@ -15,27 +43,29 @@ pub fn model_max_tokens(model: String) -> usize {
 
 /// Get the number of tokens in the text.
 #[php_function(name = "TikToken\\num_tokens")]
-pub fn num_tokens(model: String, text: String) -> usize {
-    get_bpe_from_model(model.as_str())
-        .unwrap()
+pub fn num_tokens(model: String, text: String) -> PhpResult<usize> {
+    Ok(get_bpe_from_model(model.as_str())?
         .encode_with_special_tokens(text.as_str())
-        .len()
+        .len())
 }
 
 /// Get the number of tokens in the message.
 #[php_function(name = "TikToken\\num_tokens_from_messages")]
-pub fn num_tokens_from_messages(model: String, messages: Vec<Message>) -> usize {
+pub fn num_tokens_from_messages(model: String, messages: Vec<Message>) -> PhpResult<usize> {
     let messages: Vec<_> = messages
         .iter()
-        .filter(|message| get_message_string(message, "role").is_some())
+        .filter(|message| message.string("role").is_some())
         .map(|message| ChatCompletionRequestMessage {
-            role: get_message_string(message, "role").unwrap(),
-            content: get_message_string(message, "content"),
-            name: get_message_string(message, "name"),
-            function_call: get_message_function_call(message),
+            role: message.string("role").unwrap(),
+            content: message.string("content"),
+            name: message.string("name"),
+            function_call: message.function_call(),
         })
         .collect();
-    tiktoken_rs::num_tokens_from_messages(model.as_str(), &messages).unwrap()
+    Ok(tiktoken_rs::num_tokens_from_messages(
+        model.as_str(),
+        &messages,
+    )?)
 }
 
 #[php_module]
